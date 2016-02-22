a6ba1dc7852686f25889e8577142d04a20313030
Matt Stjernholm <thew88@gmail.com>
2016-02-21 17:16:49 -0700

    adding skills works

--- app/assets/javascripts/components/_body.js.jsx
+++ app/assets/javascripts/components/_body.js.jsx
@@ -1,8 +1,26 @@
 var Body = React.createClass({
+  getInitialState() {
+    return { skills: [] }
+  },
+
+  componentDidMount() {
+    $.getJSON('/api/v1/skills.json', (response) => { this.setState({ skills: response }) });
+  },
+
+  handleSubmit(skill) {
+    var newState = this.state.skills.concat(skill);
+    this.setState( { skills: newState } )
+  },
+
   render() {
     return (
       <div>
-        <AllSkills />
+        <div>
+        <NewSkill handleSubmit={this.handleSubmit} />
+        </div>
+        <div>
+        <AllSkills skills={this.state.skills} />
+        </div>
       </div>
     )
   }
