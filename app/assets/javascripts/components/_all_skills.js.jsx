8ffe8639fc2c603207aef356b09950037b773a3e
Matt Stjernholm <thew88@gmail.com>
2016-02-21 22:37:54 -0700

    edit partially works for skill (just added _skill.js.jsx)

--- app/assets/javascripts/components/_all_skills.js.jsx
+++ app/assets/javascripts/components/_all_skills.js.jsx
@@ -3,17 +3,20 @@ var AllSkills = React.createClass({
     this.props.handleDelete(id);
   },
 
+  handleEdit() {
+    console.log('you are in edit!');
+  },
+
   render() {
     var skills = this.props.skills.map((skill) => {
-    return (
-      <div key={skill.id}>
-        <h3>{skill.name}</h3>
-        <p><strong>Level:</strong> {skill.level}</p>
-        <p>{skill.details}</p>
-        <button onClick={this.handleDelete.bind(this, skill.id)}>Delete</button>
-      </div>
-    )
-  });
+     return (
+       <div key={skill.id}>
+         <Skill skill={skill}
+                handleDelete={this.handleDelete.bind(this, skill.id)}
+                handleEdit={this.handleEdit}/>
+       </div>
+     )
+   });
 
    return(
      <div>
